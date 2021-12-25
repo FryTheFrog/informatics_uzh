5967aae562fc7f1eefeb14d276468118dd40be15
Mike Burgherr <mikeburgherr@gmail.com>
2021-12-25 21:44:53 +0100

    fix typo

--- informatics_2/C_prep/2_basics/ex_4/task_array.c
+++ informatics_2/C_prep/2_basics/ex_4/task_array.c
@@ -4,6 +4,6 @@
 // - the values in the array should be the same as their index
 
 int main() {
-    int array[5];                   // declar type of elems and size of array
+    int array[5];                   // declare type of elems and size of array
     // TODO: implement me
 }
