d3f648a8386c9215374718f05f433f6d12c3d38e
Mike Burgherr <mikeburgherr@gmail.com>
2021-12-27 14:22:01 +0100

    small correction

--- informatics_2/C_prep/2_basics/ex_5/solution/pyramid.c
+++ informatics_2/C_prep/2_basics/ex_5/solution/pyramid.c
@@ -2,14 +2,13 @@
 
 int main() {
     const int rows = 5;     // constant -> var can't be changed
-    int i, space, k = 0, count1 = 0, count = 0;    // it's possible to create multiple vars
+    int i, space, k = 0, count = 0;    // it's possible to create multiple vars
     for (i = 1; i <= rows; ++i) {                   // of the same type like this
         for (space = 1; space <= rows - i; ++space) {
             printf("  ");
-            ++count1;
         }
         while (k < 2 * i - 1) {
-            if (k <= rows - 1) {
+            if (k <= i - 1) {
                 printf("%d ", (i + k));
             }
             else {
