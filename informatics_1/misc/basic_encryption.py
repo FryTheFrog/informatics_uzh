d74adb4276ca2966a15d96d9c93fbc8af8e551ae
Mike Burgherr <mikeburgherr@gmail.com>
2022-05-04 15:13:47 +0200

    update basic_encryption

--- informatics_1/misc/basic_encryption.py
+++ informatics_1/misc/basic_encryption.py
@@ -1,33 +1,44 @@
 class Message:
-    def __init__(self, text=""):
+    def __init__(self, text="") -> None:
         self.text = text
+
+    def ask_message(self) -> None:
+        self.text = input("\nEnter message: ")
+
+    def print_message(self) -> None:
+        print("\nMessage:", self.text)
+
+    def __str__(self) -> str:
+        return self.text
+
+
+class EncryptedMessage(Message):
+    def __init__(self, text="") -> None:
+        super().__init__(text)
         self.__state = "original"
-    
-    def get_state(self):
+
+    def get_state(self) -> str:
         return self.__state
 
-    def ask_message(self):
-        self.text = input("\nEnter message: ")
-    
-    def ask_key(self):
+    def ask_key(self) -> int:
         key = int(input("Enter key: "))
         return key
-    
-    def encrypt(self, key):
+
+    def encrypt(self, key: int) -> None:
         encrypted_text = ""
         for i in range(len(self.text)):
             encrypted_text += chr(ord(self.text[i]) + key)
         self.text = encrypted_text
         self.__state = "encrypted"
-    
-    def decrypt(self, key):
+
+    def decrypt(self, key: int) -> None:
         decrypted_text = ""
         for i in range(len(self.text)):
             decrypted_text += chr(ord(self.text[i]) - key)
         self.text = decrypted_text
         self.__state = "decrypted"
-    
-    def print_message(self):
+
+    def print_message(self) -> None:
         match self.get_state():
             case "original":
                 print("\nmessage: ", self.text)
@@ -36,12 +47,14 @@ class Message:
             case "decrypted":
                 print("\ndecrypted message: ", self.text)
 
+
 def main():
-    message = Message()
+    message = EncryptedMessage()
     message.ask_message()
     message.encrypt(message.ask_key())
     message.print_message()
     message.decrypt(message.ask_key())
     message.print_message()
+
 
 main()
